f335e801ca51d1334072d193f027b87ca12475a3
qicongsheng <875881559@qq.com>
2024-04-26 19:50:08 +0800

    h5

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return redirect('/resources/html/index.html')
 
 
 @app.route('/about')
